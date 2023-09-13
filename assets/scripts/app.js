19ca014b532d810c221f5d588b4af1f4eb25c009
pershiba-velusamy06 <pershiba@elred.io>
2023-09-13 16:46:15 +0530

    if condition

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,8 +1,253 @@
-const defaultResult = 22;
+const defaultResult = 10;
 let currentResult = defaultResult;
+let logList = []
+let detailLogEntries = [];
+//currentResult = (currentResult + 10) * 3 / 2 - 1;
 
-currentResult = (currentResult + 10) * 3 / 2 - 1;
+let calculationDescription = "";
+function formObject(type, defaultResult, userInput, currentResult) {
+    let logEntry = {
+        operator: type,
+        InitialValue: defaultResult,
+        EnteredValue: userInput,
+        result: currentResult
+    }
+    detailLogEntries.push(logEntry)
 
-let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
+}
 
-outputResult(currentResult, calculationDescription);
+
+function calculation (type){
+    if(userInput.value!=0){
+        let backup = currentResult
+        console.log(currentResult)
+     
+    
+        if(type==="ADD"){
+            calculationDescription = `${currentResult} + ${parseInt(userInput.value)}`
+            currentResult += parseInt(userInput.value)
+            outputResult(currentResult, calculationDescription);
+            logList.push(calculationDescription)
+            formObject(type, backup, userInput.value, currentResult)
+    
+        }else if(type==='SUB'){
+            calculationDescription = `${currentResult} - ${parseInt(userInput.value)}`
+            currentResult -= parseInt(userInput.value)
+            outputResult(currentResult, calculationDescription);    
+            logList.push(calculationDescription)
+            formObject(type, backup, userInput.value, currentResult)
+    
+        }else if(type==='MULTIPLY'){
+            calculationDescription = `${currentResult} * ${parseInt(userInput.value)}`
+    
+            currentResult *= parseInt(userInput.value)
+            outputResult(currentResult, calculationDescription);    
+            logList.push(calculationDescription)
+            formObject(type, backup, userInput.value, currentResult)
+    
+        }else if(type==='DIVIDE'){
+            calculationDescription = `${currentResult} / ${parseInt(userInput.value)}`
+            currentResult /= parseInt(userInput.value)
+            outputResult(currentResult, calculationDescription);
+            logList.push(calculationDescription)
+        
+            formObject('DIVIDE', backup, userInput.value, currentResult)
+        }else if(type==='MODULUS'){
+            calculationDescription = `${currentResult} % ${parseInt(userInput.value)}`
+    
+            currentResult = defaultResult % parseInt(userInput.value)
+            outputResult(currentResult, calculationDescription);
+        
+            logList.push(calculationDescription)
+            formObject('MODULUs', backup, userInput.value, currentResult)
+        
+        }else if(type==='EXPONENTIAL'){
+            calculationDescription = `${currentResult} ** ${parseInt(userInput.value)}`
+    
+            currentResult = defaultResult ** parseInt(userInput.value)
+            outputResult(currentResult, calculationDescription);
+        
+            formObject('EXPONENTIAL', backup, userInput.value, currentResult)
+        }
+    }
+   
+
+
+}
+
+function add() {
+    if(userInput.value!==""&&!isNaN(userInput.value)){
+        calculation('ADD')
+    }else{
+        if(isNaN(userInput.value)){
+            alert(`${userInput.value} cannot be added with ${currentResult}`)
+            userInput.value=""
+        }else{
+            alert("please enter a value")
+        }
+    }
+ 
+    // let backup = currentResult
+    // console.log(currentResult)
+    // calculationDescription = `${currentResult} + ${parseInt(userInput.value)}`
+
+    // currentResult += parseInt(userInput.value)
+    // outputResult(currentResult, calculationDescription);
+    // formObject('ADD', backup, userInput.value, currentResult)
+
+    // logList.push(calculationDescription)
+
+    // console.log(logList, "logList")
+    // console.log(detailLogEntries, "detailLogEntries")
+}
+
+function subtract() {
+    if(userInput.value!==""&&!isNaN(userInput.value)){
+        calculation('SUB')
+    }else{
+        if(isNaN(userInput.value)){
+            alert(`${userInput.value} cannot be subracted from ${currentResult}`)
+            userInput.value=""
+        }else{
+            alert("please enter a value")
+        }
+    }
+ 
+    // let backup = currentResult
+    // console.log(currentResult)
+    // calculationDescription = `${currentResult} - ${parseInt(userInput.value)}`
+
+    // currentResult -= parseInt(userInput.value)
+    // outputResult(currentResult, calculationDescription);
+
+    // formObject('SUB', backup, userInput.value, currentResult)
+
+    // logList.push(calculationDescription)
+
+   
+    // console.log(logList, "logList")
+    // console.log(detailLogEntries, "detailLogEntries")
+}
+
+function multiply() {
+
+    if(userInput.value!==""&&!isNaN(userInput.value)){
+        calculation('MULTIPLY')
+    }else{
+        if(isNaN(userInput.value)){
+            alert(`${userInput.value} cannot be multiplied with ${currentResult}`)
+            userInput.value=""
+        }else{
+            alert("please enter a value")
+        }
+    }
+    // let backup = currentResult
+    // calculationDescription = `${currentResult} * ${parseInt(userInput.value)}`
+
+    // currentResult *= parseInt(userInput.value)
+    // outputResult(currentResult, calculationDescription);
+
+    // formObject('MULTIPLY', backup, userInput.value, currentResult)
+
+    // logList.push(calculationDescription)
+
+   
+    // console.log(logList, "logList")
+    // console.log(detailLogEntries, "detailLogEntries")
+}
+
+function divide() {
+   
+    if(userInput.value!==""&&!isNaN(userInput.value)){
+        calculation('DIVIDE')
+    }else{
+        if(isNaN(userInput.value)){
+            alert(`${userInput.value} cannot be divided by ${currentResult}`)
+            userInput.value=""
+        }else{
+            alert("please enter a value")
+        }
+    }
+    // let backup = currentResult
+    // calculationDescription = `${currentResult} / ${parseInt(userInput.value)}`
+
+    // currentResult /= parseInt(userInput.value)
+    // outputResult(currentResult, calculationDescription);
+    // logList.push(calculationDescription)
+
+    // formObject('DIVIDE', backup, userInput.value, currentResult)
+   
+    // console.log(logList, "logList")
+    // console.log(detailLogEntries, "detailLogEntries")
+}
+function modulus() {
+   
+
+    if(userInput.value!==""&&!isNaN(userInput.value)){
+        calculation('MODULUS')
+    }else{
+        if(isNaN(userInput.value)){
+            alert(`${userInput.value} cannot be calculated with ${currentResult}`)
+            userInput.value=""
+        }else{
+            alert("please enter a value")
+        }
+    }
+
+    // let backup = currentResult
+    // calculationDescription = `${currentResult} % ${parseInt(userInput.value)}`
+
+    // currentResult = defaultResult % parseInt(userInput.value)
+    // outputResult(currentResult, calculationDescription);
+
+    // logList.push(calculationDescription)
+    // formObject('MODULUs', backup, userInput.value, currentResult)
+
+   
+    // console.log(logList, "logList")
+    // console.log(detailLogEntries, "detailLogEntries")
+}
+function exponentional() {
+ 
+    if(userInput.value!==""&&!isNaN(userInput.value)){
+        calculation('EXPONENTIAL')
+    }else{
+        if(isNaN(userInput.value)){
+            alert(`${userInput.value} cannot be calculated with ${currentResult}`)
+            userInput.value=""
+        }else{
+            alert("please enter a value")
+            
+        }
+    }
+    // let backup = currentResult
+    // calculationDescription = `${currentResult} ** ${parseInt(userInput.value)}`
+
+    // currentResult = defaultResult ** parseInt(userInput.value)
+    // outputResult(currentResult, calculationDescription);
+
+    // formObject('EXPONENTIAL', backup, userInput.value, currentResult)
+
+    // logList.push(calculationDescription)
+
+   
+    // console.log(logList, "logList")
+    // console.log(detailLogEntries, "detailLogEntries")
+}
+function ResetAll() {
+    currentResult = 0
+    calculationDescription = ""
+    userInput.value=""
+    currentCalculationOutput.value=0
+    outputResult(currentResult, calculationDescription);
+   
+}
+
+
+addBtn.addEventListener('click', add)
+subtractBtn.addEventListener('click', subtract)
+multiplyBtn.addEventListener('click', multiply)
+divideBtn.addEventListener('click', divide)
+modulo.addEventListener('click', modulus)
+expo.addEventListener('click', exponentional)
+Rest.addEventListener('click', ResetAll)
--- assets/scripts/vendor.js
+++ assets/scripts/vendor.js
@@ -3,11 +3,16 @@ const addBtn = document.getElementById('btn-add');
 const subtractBtn = document.getElementById('btn-subtract');
 const multiplyBtn = document.getElementById('btn-multiply');
 const divideBtn = document.getElementById('btn-divide');
+const modulo = document.getElementById('btn-modulo');
+const expo = document.getElementById('btn-expo');
+const Rest = document.getElementById('btn-Rest');
 
 const currentResultOutput = document.getElementById('current-result');
 const currentCalculationOutput = document.getElementById('current-calculation');
+const currentCalculationOutputhead = document.getElementById("current-calculation-head");
 
 function outputResult(result, text) {
   currentResultOutput.textContent = result;
-  currentCalculationOutput.textContent = text;
+   currentCalculationOutput.textContent = text;
+   currentCalculationOutputhead.style='display:block'
 }
